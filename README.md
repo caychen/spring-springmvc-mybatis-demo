be05d3e044ac1119e7747affb81534c1fc7157c2
Cay <412425870@qq.com>
2017-07-30 18:44:40 +0800

    添加项目源码

---
+++ README.md
@@ -0,0 +1,2 @@
+#spring-springmvc-mybatis-demo
+spring-springmvc-mybatis Demo
---
+++ src/main/java/org/com/cay/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package org.com.cay.service.impl;
+
+import java.util.List;
+
+import org.com.cay.dao.IUserMapper;
+import org.com.cay.entity.User;
+import org.com.cay.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+	private IUserMapper userMapper;
+	
+	@Override
+	public void saveUser(User user) {
+		// TODO Auto-generated method stub
+		user.setPassword("111111");
+		userMapper.saveUser(user);
+	}
+
+	@Override
+	public List<User> list() {
+		// TODO Auto-generated method stub
+		return userMapper.list();
+	}
+
+	@Override
+	public User getUser(Integer id) {
+		// TODO Auto-generated method stub
+		return userMapper.getUser(id);
+	}
+
+}
